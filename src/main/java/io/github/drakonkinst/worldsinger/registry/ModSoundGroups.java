97015576253a420accb7d3b7ccab7b58efca6d4e
Drakonkinst <drakonkinstmc@gmail.com>
2024-10-13 23:36:09 -0700

    Some texture updates

--- src/main/java/io/github/drakonkinst/worldsinger/registry/ModSoundGroups.java
+++ src/main/java/io/github/drakonkinst/worldsinger/registry/ModSoundGroups.java
@@ -37,7 +37,7 @@ public final class ModSoundGroups {
     public static final BlockSoundGroup ROSEITE = BlockSoundGroup.GLASS;
     public static final BlockSoundGroup CRIMSON_GROWTH = BlockSoundGroup.DRIPSTONE_BLOCK;
     public static final BlockSoundGroup CRIMSON_SPINE = BlockSoundGroup.POINTED_DRIPSTONE;
-    public static final BlockSoundGroup VERDANT_VINE_BRANCH = BlockSoundGroup.WOOD;
+    public static final BlockSoundGroup VERDANT_VINE_BRANCH = BlockSoundGroup.GRASS;
     public static final BlockSoundGroup VERDANT_VINE_SNARE = BlockSoundGroup.GRASS;
     public static final BlockSoundGroup TWISTING_VERDANT_VINES = BlockSoundGroup.WEEPING_VINES;
     public static final BlockSoundGroup MIDNIGHT_ESSENCE = BlockSoundGroup.SOUL_SAND;
