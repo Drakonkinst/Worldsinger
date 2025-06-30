5bc8d54afab317c226e6f29262ae1466a8bc72f5
Drakonkinst <drakonkinstmc@gmail.com>
2025-06-30 11:09:21 -0700

    Move mixin to callback

--- src/client/java/io/github/drakonkinst/worldsinger/registry/ModHudElements.java
+++ src/client/java/io/github/drakonkinst/worldsinger/registry/ModHudElements.java
@@ -5,8 +5,14 @@ import io.github.drakonkinst.worldsinger.api.ModAttachmentTypes;
 import io.github.drakonkinst.worldsinger.effect.ModStatusEffects;
 import io.github.drakonkinst.worldsinger.event.thirst.ThirstEvents;
 import io.github.drakonkinst.worldsinger.gui.thirst.ThirstStatusBar;
+import io.github.drakonkinst.worldsinger.item.component.CannonballComponent;
+import io.github.drakonkinst.worldsinger.item.component.CannonballComponent.CannonballContent;
+import java.util.List;
+import net.fabricmc.fabric.api.client.rendering.v1.DrawItemStackOverlayCallback;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.util.Colors;
 
 public class ModHudElements {
 
@@ -23,6 +29,19 @@ public class ModHudElements {
 
     @SuppressWarnings("UnstableApiUsage")
     public static void registerHudEventHandlers() {
+        DrawItemStackOverlayCallback.EVENT.register(((context, textRenderer, stack, x, y) -> {
+            CannonballComponent component = stack.get(ModDataComponentTypes.CANNONBALL);
+            if (component == null || !component.core().isFillable() || component.contents()
+                    .isEmpty()) {
+                return;
+            }
+            context.getMatrices().pushMatrix();
+            List<CannonballContent> contents = component.contents();
+            drawCannonballContentBar(context, contents, 0, x, y, 2, 6);
+            drawCannonballContentBar(context, contents, 1, x, y, 6, 10);
+            drawCannonballContentBar(context, contents, 2, x, y, 10, 14);
+            context.getMatrices().popMatrix();
+        }));
         // TODO: Add option for showing an item when consuming it modifies thirst?
         // TODO: Add option for showing when thirst decreases unnaturally?
         ThirstEvents.VISIBLE_PREDICATE.register(player -> showThirstAlways);
@@ -37,5 +56,15 @@ public class ModHudElements {
         ThirstEvents.VISIBLE_PREDICATE.register(player ->
                 player.getAttachedOrCreate(ModAttachmentTypes.MIDNIGHT_AETHER_BOND).getBondCount()
                         > 0);
+    }
+
+    private static void drawCannonballContentBar(DrawContext context,
+            List<CannonballContent> contents, int index, int x, int y, int from, int to) {
+        if (index >= contents.size()) {
+            return;
+        }
+        int startY = y + 16 - to;
+        int endY = y + 16 - from;
+        context.fill(x, startY, x + 1, endY, contents.get(index).getBarColor() | Colors.BLACK);
     }
 }
