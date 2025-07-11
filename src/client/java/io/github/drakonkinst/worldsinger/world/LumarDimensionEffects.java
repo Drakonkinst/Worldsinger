820df69d0c9b8d51ff4e3db6ee7d7caa0166bc0a
Drakonkinst <drakonkinstmc@gmail.com>
2025-07-11 07:30:44 -0700

    Fix sunsets not rendering

--- src/client/java/io/github/drakonkinst/worldsinger/world/LumarDimensionEffects.java
+++ src/client/java/io/github/drakonkinst/worldsinger/world/LumarDimensionEffects.java
@@ -25,6 +25,8 @@ package io.github.drakonkinst.worldsinger.world;
 
 import io.github.drakonkinst.worldsinger.registry.ModClientEnums;
 import net.minecraft.client.render.DimensionEffects;
+import net.minecraft.util.math.ColorHelper;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 
 public class LumarDimensionEffects extends DimensionEffects {
@@ -37,6 +39,20 @@ public class LumarDimensionEffects extends DimensionEffects {
     public Vec3d adjustFogColor(Vec3d color, float sunHeight) {
         return color.multiply(sunHeight * 0.94f + 0.06f, sunHeight * 0.94f + 0.06f,
                 sunHeight * 0.91f + 0.09f);
+    }
+
+    @Override
+    public int getSkyColor(float skyAngle) {
+        float f = MathHelper.cos(skyAngle * (float) (Math.PI * 2));
+        float g = f / 0.4F * 0.5F + 0.5F;
+        float h = MathHelper.square(1.0F - (1.0F - MathHelper.sin(g * (float) Math.PI)) * 0.99F);
+        return ColorHelper.fromFloats(h, g * 0.3F + 0.7F, g * g * 0.7F + 0.2F, 0.2F);
+    }
+
+    @Override
+    public boolean isSunRisingOrSetting(float skyAngle) {
+        float f = MathHelper.cos(skyAngle * (float) (Math.PI * 2));
+        return f >= -0.4F && f <= 0.4F;
     }
 
     @Override
